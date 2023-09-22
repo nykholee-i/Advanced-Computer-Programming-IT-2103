4929970e7d220ae205f6868efa9b495bb8d545b3
ISIP, Nykhole M <145734108+nykholee-i@users.noreply.github.com>
2023-09-22 18:39:44 +0800

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,9 @@
+name = input('Enter your name: ')
+age = int (input('Enter your age: '))
+address = input('Enter your Address: ')
+
+print("Information Received")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Address: {address}")
+
